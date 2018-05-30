53504336c7d52c581c0c709bd34e6be27e72cf11
mxbrandi <max.brandi@mail.de>
2018-05-30 20:31:12 +0200

    Refactor: Results are now saved as JSON String such that saving a PracticeRoutineExecution object with SugarORM is made possible

--- app/src/main/java/com/mxwlone/snookerroutines/lib/PracticeRoutineExecution.java
+++ app/src/main/java/com/mxwlone/snookerroutines/lib/PracticeRoutineExecution.java
@@ -1,6 +1,12 @@
 package com.mxwlone.snookerroutines.lib;
 
+import android.util.Log;
+
 import com.orm.SugarRecord;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import java.util.ArrayList;
 import java.util.Date;
@@ -9,7 +15,7 @@ import java.util.List;
 public final class PracticeRoutineExecution extends SugarRecord {
     private int practiceRoutineId;
     private Date date;
-    private List<Integer> results = new ArrayList<>();
+    private String results;
 
     public PracticeRoutineExecution() {
     }
@@ -31,29 +37,41 @@ public final class PracticeRoutineExecution extends SugarRecord {
         return date;
     }
 
-    public void addResult(int result) {
-        results.add(result);
+    public void addResult(int index, int result) {
+        try {
+            JSONObject jsonResults = new JSONObject(this.results);
+            jsonResults.put(Integer.toString(index), result);
+        } catch (JSONException e) {
+            Log.e("ERROR", e.getMessage());
+        }
     }
 
-    public List<Integer> getResults() {
-        return results;
+    public JSONObject getResults() {
+        try {
+            return new JSONObject(this.results);
+        } catch (JSONException e) {
+            Log.e("ERROR", e.getMessage());
+        }
+
+        return new JSONObject();
     }
 
     public boolean isEmpty() {
         return results.isEmpty();
     }
 
-    public float getAverageResult() {
-        float averageResult = 0f;
-
-        if (results.size() == 0) {
-            return averageResult;
-        }
-
-        for (int result : results) {
-            averageResult += result;
-        }
-
-        return averageResult / results.size();
-    }
+    // TODO calculate average result of JSON results
+//    public float getAverageResult() {
+//        float averageResult = 0f;
+//
+//        if (results.size() == 0) {
+//            return averageResult;
+//        }
+//
+//        for (int result : results) {
+//            averageResult += result;
+//        }
+//
+//        return averageResult / results.size();
+//    }
 }
--- app/src/test/java/com/mxwlone/snookerroutines/ExampleUnitTest.java
+++ app/src/test/java/com/mxwlone/snookerroutines/ExampleUnitTest.java
@@ -1,5 +1,6 @@
 package com.mxwlone.snookerroutines;
 
+import org.json.JSONObject;
 import org.junit.Test;
 
 import static org.junit.Assert.*;
@@ -13,5 +14,20 @@ public class ExampleUnitTest {
     @Test
     public void addition_isCorrect() throws Exception {
         assertEquals(4, 2 + 2);
+    }
+
+    @Test
+    public void JSONTest() throws Exception {
+        JSONObject jsonObject = new JSONObject();
+        jsonObject.put("1", 10);
+        jsonObject.put("1", 20);
+        jsonObject.put("3", 20);
+
+        String results = jsonObject.toString();
+        System.out.println(results);
+
+        JSONObject jsonObject1 = new JSONObject(results);
+        jsonObject1.put("3", 30);
+        System.out.println(jsonObject1.toString());
     }
 }
