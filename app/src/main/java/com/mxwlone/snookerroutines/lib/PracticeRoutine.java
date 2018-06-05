804f56f5f1831919b909ec5b4218674b3831682d
mxwlone <1ax@gmx.net>
2018-06-05 23:24:19 +0200

    Add parameter maxScore:int and tag for logging
    The `maxScore` parameter is the maximum achievable score in a
    practice routine with style `Style.DISCRETE` and Integer.MAX_VALUE
    in a practice routine with style `Style.CONTINUOUS`

--- app/src/main/java/com/mxwlone/snookerroutines/lib/PracticeRoutine.java
+++ app/src/main/java/com/mxwlone/snookerroutines/lib/PracticeRoutine.java
@@ -1,30 +1,43 @@
 package com.mxwlone.snookerroutines.lib;
 
+import android.util.Log;
+
 import java.io.Serializable;
 
 public final class PracticeRoutine implements Serializable {
+    private static final String TAG = PracticeRoutine.class.getSimpleName();
 
     private String imageUrl;
     private String name;
     private String description;
-    private String scoring;
+    private String scoringDescription;
     private Difficulty difficulty;
     private Style style;
+    private int maxScore;
     private Tag[] tags;
 
-    PracticeRoutine(String name, Difficulty difficulty, Style style, Tag[] tags, String description,
-                    String scoring, String imageUrl) {
+    PracticeRoutine(String name, Difficulty difficulty, Style style, int maxScore, Tag[] tags,
+                    String description, String scoringDescription, String imageUrl) {
         this.name = name;
         this.difficulty = difficulty;
         this.style = style;
         this.description = description;
-        this.scoring = scoring;
+        this.scoringDescription = scoringDescription;
         this.imageUrl = imageUrl;
         this.tags = tags;
+        this.maxScore = maxScore;
+
+        if (style == Style.CONTINUOUS && maxScore != Integer.MAX_VALUE) {
+            Log.w(TAG, "Max score set for non-discrete practice routine!");
+        }
     }
 
-    public String getScoring() {
-        return scoring;
+    public int getMaxScore() {
+        return maxScore;
+    }
+
+    public String getScoringDescription() {
+        return scoringDescription;
     }
 
     public Style getStyle() {
